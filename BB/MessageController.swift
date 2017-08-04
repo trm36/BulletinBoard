5b95db5c27582d9c64a6b145fcd999aa787fb6c6
Taylor Mott <trm36@humboldt.edu>
2017-08-03 19:33:33 -0600

    message controller complete
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- BB/MessageController.swift
+++ BB/MessageController.swift
@@ -9,11 +9,22 @@
 import Foundation
 import CloudKit
 
+extension MessageController {
+    static let didRefreshNotification = Notification.Name("didRefreshNotification")
+}
+
 class MessageController {
     
     static let shared = MessageController()
     
-    private(set) var messages: [Message] = []
+    private(set) var messages: [Message] = [] {
+        didSet {
+            DispatchQueue.main.async(execute: {
+                NotificationCenter.default.post(name: MessageController.didRefreshNotification, object: self)
+            })
+        }
+    }
+    
     private let cloudKitManager = CloudKitManager()
     
     func post(_ message: Message, completion: @escaping ((Error?) -> Void) = {_ in }) {
@@ -31,6 +42,48 @@ class MessageController {
             }
         }
     }
+    
+    private func refresh(completion: @escaping ((Error?) -> Void) = {_ in }) {
+        let sortDescriptors = [NSSortDescriptor(key: Message.dateKey, ascending: false)]
+        
+        cloudKitManager.fetchRecords(ofType: Message.recordType, sortDescriptors: sortDescriptors) { (records: [CKRecord]?, error: Error?) in
+            
+            defer { completion(error) }
+            
+            if let error = error {
+                NSLog("Error fetching messages: \(error)")
+            } else {
+                guard let records = records else { return }
+                
+                self.messages = records.flatMap({ Message(cloudKitRecord: $0) })
+            }
+        }
+    }
+    
+    init() {
+        refresh()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
