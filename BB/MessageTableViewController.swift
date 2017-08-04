e9480e04674c14d687f03a645441fb83ecaff7f6
Taylor Mott <trm36@humboldt.edu>
2017-08-03 19:56:13 -0600

    IBOutlet/IBAction
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- BB/MessageTableViewController.swift
+++ BB/MessageTableViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class MessageTableViewController: UITableViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func postButtonTapped() {
+        guard let messageText = textField.text else { return }
+        textField.resignFirstResponder()
+        let message = Message(messageText: messageText, date: Date())
+        MessageController.shared.post(message)
+    }
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageController.shared.messages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        
+        let messages = MessageController.shared.messages
+        let message = messages[indexPath.row]
+        
+        cell.textLabel?.text = message.messageText
+        cell.detailTextLabel?.text = dateFormatter.string(from: message.date)
+        
+        return cell
     }
 
 
