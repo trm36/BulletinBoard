680563793b348ef11ae51c83c10ef69fa49c8c1c
Taylor Mott <trm36@humboldt.edu>
2017-08-03 18:28:51 -0600

    strings
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ BB/CloudKitManager.swift
@@ -0,0 +1,16 @@
+//
+//  CloudKitManager.swift
+//  BB
+//
+//  Created by Taylor Mott on 03-Aug-17.
+//  Copyright © 2017 Mott Applications. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+private let CreatorUserRecordIDKey = "creatorUserRecordID"
+private let LastModifiedUserRecordIDKey = "creatorUserRecordID"
+private let CreationDateKey = "creationDate"
+private let ModificationDateKey = "modificationDate"
+
