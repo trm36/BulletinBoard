dc987483365e959856f0f3e3a30d7eab8a4d9242
Taylor Mott <trm36@humboldt.edu>
2017-08-03 18:22:45 -0600

    Initial Commit
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

---
+++ BB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BB
+//
+//  Created by Taylor Mott on 03-Aug-17.
+//  Copyright © 2017 Mott Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
