11d86e123c5ff9db10c0c49945b9513cad6172f1
Taylor Mott <trm36@humboldt.edu>
2017-08-03 19:05:42 -0600

    To/From CKRecord
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- BB/Message.swift
+++ BB/Message.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CloudKit
 
 struct Message {
 	let messageText: String
@@ -20,6 +21,24 @@ struct Message {
 extension Message {
     static var messageTextKey: String { return "MessageText" }
     static var dateKey: String { return "Date" }
+    
+    static var recordType: String { return "Message" }
+    
+    
+    var cloudKitRecord: CKRecord {
+        let record = CKRecord(recordType: Message.recordType)
+        record[Message.messageTextKey] = messageText as CKRecordValue
+        record[Message.dateKey] = date as NSDate
+        return record
+    }
+    
+    init?(cloudKitRecord: CKRecord) {
+        guard let messageText = cloudKitRecord[Message.messageTextKey] as? String,
+        let date = cloudKitRecord[Message.dateKey] as? Date,
+            cloudKitRecord.recordType == Message.recordType else { return nil }
+        
+        self.init(messageText: messageText, date: date)
+    }
     
     
 }
